4f3f1c24de12e3f2d732bda344e1a25b78291d37
Chris <northodontist@gmail.com>
2019-04-01 20:08:30 -0500

    filled in more parts of the form

--- main.py
+++ main.py
@@ -5,34 +5,41 @@ app.config['DEBUG'] = True
 
 form = """
 <!DOCTYPE html>
-
-<html>
-    <head>
-        <style>
-            form {
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
-            }
-            textarea {
-                margin: 10px;
-                width: 540px;
-                height: 120px;
-            }
-        </style>
-    </head>
-    <body>
-
-    </body>
-</html>
-
+    <html>
+        <head>
+            <style>
+                form {
+                    background-color: #eee;
+                    padding: 20px;
+                    margin: 0 auto;
+                    width: 540px;
+                    font: 16px sans-serif;
+                    border-radius: 10px;
+                    }
+                textarea {
+                    margin: 10px;
+                    width: 540px;
+                    height: 120px;
+                    }
+            </style>
+        </head>
+        
+        <body>
+            <form method="post">
+                <input type="text" name="rot"/>
+                <textarea name="text">
+                    <label>
+                        Rotation Amount
+                    </label>
+                </textarea>
+            </form>
+        </body>
+    </html>
+</form>
 """
 
 @app.route("/")
 def index():
-
+    return form
 
 app.run()
